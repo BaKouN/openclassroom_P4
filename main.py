e2eb35237b166ca36b836c44e4ce5eba41350401
Aurélien <aurelien.lebray@gmail.com>
2026-03-27 18:56:59 +0100

    Lesson 27/03/2026

---
+++ main.py
@@ -0,0 +1,9 @@
+from models.player import Player
+from models.tournament import Tournament
+
+def main():
+    player = Player()
+    # tournament = Tournament()
+
+if __name__ == '__main__':
+    main()
--- models/player.py
+++ models/player.py
@@ -1,11 +1,17 @@
 class Player:
     """Represente un joueur d'echecs."""
 
-    def __init__(self, last_name, first_name, birth_date, national_id):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.birth_date = birth_date
-        self.national_id = national_id
+    def __init__(self, last_name : str=None, **kwargs):
+        self.last_name = last_name or "Lebray"
+        # self.first_name = first_name
+        # self.birth_date = birth_date
+        # self.national_id = national_id
+        
+        self.last_name = kwargs.get("last_name", None)
+        if not self.last_name or type(self.last_name) != str:
+            print(
+                f"Last name should be defined as a string, currently {self.last_name} ({type(self.last_name)})"
+            )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.national_id})"
